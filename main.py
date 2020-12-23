c497d8e1f0885a80c2fbdae118816a856f298e60
Sanskar Gyawali <sanskargyawali@Sanskars-MBP.attlocal.net>
2020-12-22 18:54:29 -0800

    final commit

--- main.py
+++ main.py
@@ -32,7 +32,3 @@ def smallest_diff_goals(filename):
 
 min_temp_spread("w_data.dat")
 smallest_diff_goals("soccer.dat")
-
-# The way I wrote the second program was influenced by the first. The thought about writing the first program by just splitting the line and then getting the numbers to take the difference,
-# but then I realized that some of the numbers had '*' in them so I decided to use regular expression to only take ints/floats digits from the lines. 
-# My second program followed the same pattern where I only took digits using regular expression and found the difference. Therefore, it was influenced by the first.
